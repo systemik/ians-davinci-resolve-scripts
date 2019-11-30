0aa9353f9b7f0341eeacf6f03877567b798f17e1
Ian Haigh <mail@ianhaigh.com>
2019-11-30 19:41:54 +1100

    renderBySuffix oh yeah

--- scripts/renderBySuffix.py
+++ scripts/renderBySuffix.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
+import os
+import argparse
 import ianresolvelib as resolve
+
+renderPreset = "MP4 1080p"
+
+parser = argparse.ArgumentParser(description="Add timelines to Resolve's render queue by specifying a suffix.")
+parser.add_argument("-d", "--dest", help="Destination directory for the renders")
+parser.add_argument("suffix")
+args = parser.parse_args()
+
+# print(args.dest)
+if not args.dest:
+    args.dest = os.getcwd()
 
 resolve.DeleteAllRenderJobs()
 
-for tl in resolve.GetTimelinesBySuffix("06"):
-    resolve.AddTimelineToRender(tl, "MP4 1080p", "/Volumes/22-ketchup/work/QWND001 Values/support/9_renders/preview_renders/v06")
-
-# print(project.GetRenderFormats())
+for tl in resolve.GetTimelinesBySuffix(args.suffix):
+    resolve.AddTimelineToRender(tl, renderPreset, args.dest)
