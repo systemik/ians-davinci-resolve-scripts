65f89974cc07983574247db98adedad2a5edfce5
Ian Haigh <mail@ianhaigh.com>
2019-12-11 10:12:54 +1100

    lil tweaks to command line switches

--- renameTimelines.py
+++ renameTimelines.py
@@ -6,9 +6,10 @@ import ResolveLib.ianresolvelib as resolve
 import re
 
 parser = argparse.ArgumentParser(description="Add timelines to Resolve's render queue by specifying a suffix.")
-parser.add_argument("search", help="Destination directory for the renders")
-parser.add_argument("replace", help="Keep the queue instead of deleting it")
-parser.add_argument("-r", "--regexp")
+parser.add_argument("search", help="Search term, adoy")
+parser.add_argument("replace", help="Replace term innit")
+# parser.add_argument("-r", "--regexp", help="")
+# parser.add_argument("-t", "--test", help="Check out what will happen before doing it")
 args = parser.parse_args()
 
 p = re.compile(args.search)
--- renderByRegexp.py
+++ renderByRegexp.py
@@ -9,12 +9,16 @@ renderPreset = "MP4 1080p"
 parser = argparse.ArgumentParser(description="Add timelines to Resolve's render queue by specifying a suffix.")
 parser.add_argument("-d", "--dest", help="Destination directory for the renders")
 parser.add_argument("-k", "--keep", help="Keep the queue instead of deleting it")
+parser.add_argument("-p", "--preset", help="Override the default preset")
 parser.add_argument("regexp")
 args = parser.parse_args()
 
 # print(args.dest)
 if not args.dest:
     args.dest = os.getcwd()
+
+if args.preset:
+    renderPreset = args.preset
 
 if not args.keep:
     resolve.DeleteAllRenderJobs()
