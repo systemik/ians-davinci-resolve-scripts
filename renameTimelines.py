c6a746aa55f12dce82d5ee14321a14a121bd9826
Ian Haigh <mail@ianhaigh.com>
2020-01-15 12:07:32 +1100

    slight tweak to renameTimelines

--- renameTimelines.py
+++ renameTimelines.py
@@ -8,7 +8,7 @@ import re
 parser = argparse.ArgumentParser(description="Rename Resolve timelines by specifying a regular expression.")
 parser.add_argument("search", help="Search term, adoy")
 parser.add_argument("replace", help="Replace term innit")
-# parser.add_argument("-r", "--regexp", help="")
+parser.add_argument("-q", "--quiet", action="store_true", help="Ssshhhh")
 # parser.add_argument("-t", "--test", help="Check out what will happen before doing it")
 args = parser.parse_args()
 
@@ -16,6 +16,8 @@ p = re.compile(args.search)
 for tl in resolve.GetTimelinesByRegexp(args.search):
     # resolve.AddTimelineToRender(tl, renderPreset, args.dest)
 
-    x = p.sub(args.replace, tl.GetName())
-    print(x)
+    tlName = tl.GetName()
+    x = p.sub(args.replace, tlName)
+    if not args.quiet:
+        print(f'{tlName} > {x}')
     tl.SetName(x)
