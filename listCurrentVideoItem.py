a0c6c9dd5fea68f00ff11885ac884c7d32943361
Ian Haigh <mail@ianhaigh.com>
2020-10-20 16:59:08 +1100

    no message

--- listCurrentVideoItem.py
+++ listCurrentVideoItem.py
@@ -2,6 +2,10 @@
 
 import ResolveLib.ianresolvelib as r
 
+tl = r.project.GetCurrentTimeline()
+print(f'Timeline: {tl.GetName()}')
+markers = tl.GetMarkers()
+
 item = r.project.GetCurrentTimeline().GetCurrentVideoItem()
 print(item.GetName())
 
