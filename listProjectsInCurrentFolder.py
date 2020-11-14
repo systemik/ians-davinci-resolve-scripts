61ab7ff9d34b0417bf5efd476e0f136932a1d12c
Ian Haigh <mail@ianhaigh.com>
2020-11-14 12:06:21 +1100

    tryna get some recursion happening. This works but the list is flat, I want folders IN folders dammit

--- listProjectsInCurrentFolder.py
+++ listProjectsInCurrentFolder.py
@@ -1,5 +1,53 @@
 #!/usr/bin/env python3
 
-import ResolveLib.ianresolvelib as r
+import json
 
-print(r.projectManager.GetProjectsInCurrentFolder())
+def main():
+    """
+    list all the folders and projects from the root folder in resolve
+    """
+    import ResolveLib.ianresolvelib as r
+    
+    root = {}
+    
+    def listProjects(folder=None):
+        thisFolder = {}
+        if folder is not None:
+            r.projectManager.OpenFolder(folder)
+            print(f'In the folder {folder}, I found the following folders')
+            thisFolder["folder"] = folder
+        else:
+            thisFolder["folder"] = "root"
+
+
+        for currentFolder in r.projectManager.GetFolderListInCurrentFolder():
+            print(currentFolder)
+            listProjects(currentFolder)
+        
+        
+        thisFolder["projects"] = r.projectManager.GetProjectsInCurrentFolder()
+        r.projectManager.GotoParentFolder()
+        
+        root[folder] = thisFolder
+        
+        return
+
+
+    r.projectManager.GotoRootFolder()
+    
+    listProjects()
+    # print(r.projectManager.GetProjectsInCurrentFolder())
+
+    # for currentFolder in r.projectManager.GetFolderListInCurrentFolder():
+        # listProjects(currentFolder)
+        
+    # root["projects"] = r.projectManager.GetProjectsInCurrentFolder()
+    
+    # r.projectManager.OpenFolder("_archive")
+    
+    # root["_archive"] = r.projectManager.GetProjectsInCurrentFolder()
+    
+    print(json.dumps(root))
+
+if __name__ == "__main__":
+    main()
