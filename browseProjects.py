9d99f3fd553a41b9157a2ad0250e6906b11d1a5c
Ian Haigh <mail@ianhaigh.com>
2020-11-14 15:51:55 +1100

    YES i can read the json

--- browseProjects.py
+++ browseProjects.py
@@ -15,28 +15,40 @@ def main():
     parser.add_argument("pathToJson", type=str, help="Path to the json")
 
     args = parser.parse_args()
-    
+
     f = open(args.pathToJson)
     data = json.load(f)
-    
-    indent = 0
-    def recurse(obj):
-        for f in obj:
-            # nonlocal indent
-            # print("NEW THINGIE")
-            # print("_" * 60, "\nNEW THINGIE", "_" * 60, sep="\n")
-            # print(obj)
-            # print("\t" * indent, f)
-            # indent += 1
-            print(f)
-            if f is not "_":
-                recurse(f)
-            # indent -= 1
-        
-    
-    recurse(data["root"])
+
+    # indent = 0
+    # currentPath = ""
+
+    def recurse(obj, currentPath):
+        """
+        obj is a dict from the json
+        currentPath is a list to keep track of where we are
+        """
+        for f in obj.keys():
+            if f == "projects":
+                for y in obj["projects"].values():
+                    print(y)
+                return
+            else:
+                currentPath.append(f)
+                print("/".join(currentPath))
+
+            # better make sure it's a dict before recursion
+            if isinstance(obj[f], dict):
+                recurse(obj[f], currentPath)
+
+            # going back up a level
+            currentPath.pop()
+
+        return
+
+    recurse(data["root"], [""])
+
     # for item in data["root"]:
-        # print(item)
+    # print(item)
 
 
 if __name__ == "__main__":
